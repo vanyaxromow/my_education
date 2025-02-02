e3ddcc10d14723aa85dbeba1293903a08607cada
vanyaxromow <vanya.xromow@yandex.ru>
2025-02-02 10:32:10 +0300

    Update and rename 2.1.py to 2.1 public, private, protected.py

--- oop_Balakirev_lessons/2.1 public, private, protected.py
+++ oop_Balakirev_lessons/2.1 public, private, protected.py
@@ -18,28 +18,28 @@
 # P.S. На экран ничего выводить не нужно.
 
 
-# class Clock:
-#     def __init__(self, time):
-#         self.__time = 0
-#         if self.set_time(time):
-#             self.__time = time
-#
-#     def set_time(self, tm):
-#         if self.__check_time(tm):
-#             self.__time = tm
-#
-#     def get_time(self):
-#         return self.__time
-#
-#     @classmethod
-#     def __check_time(cls, tm):
-#         if type(tm) is int and 0 <= tm <= 100000:
-#             return True
-#         return False
-#
-#
-# clock = Clock(4530)
-# print(clock.get_time())
+class Clock:
+    def __init__(self, time):
+        self.__time = 0
+        if self.set_time(time):
+            self.__time = time
+
+    def set_time(self, tm):
+        if self.__check_time(tm):
+            self.__time = tm
+
+    def get_time(self):
+        return self.__time
+
+    @classmethod
+    def __check_time(cls, tm):
+        if type(tm) is int and 0 <= tm <= 100000:
+            return True
+        return False
+
+
+clock = Clock(4530)
+print(clock.get_time())
 
 
 # Подвиг 4. Объявите класс с именем Money и определите в нем следующие переменные и методы:
@@ -65,34 +65,34 @@
 # m2 = mn_2.get_money()    # 120
 
 
-# class Money:
-#     def __init__(self, money):
-#         self.__money = 0
-#         if self.__check_money(money):
-#             self.__money = money
-#
-#     def set_money(self, money):
-#         if self.__check_money(money):
-#             self.__money = money
-#
-#     def get_money(self):
-#         return self.__money
-#
-#     def add_money(self, mn):
-#         self.__money += mn.__money
-#
-#     @classmethod
-#     def __check_money(cls, money):
-#         return type(money) == int and 0 <= money
-#
-#
-# mn_1 = Money(10)
-# mn_2 = Money(20)
-# mn_1.set_money(100)
-# mn_2.add_money(mn_1)
-# m1 = mn_1.get_money()    # 100
-# m2 = mn_2.get_money()    # 120
-# print(m1, m2)
+class Money:
+    def __init__(self, money):
+        self.__money = 0
+        if self.__check_money(money):
+            self.__money = money
+
+    def set_money(self, money):
+        if self.__check_money(money):
+            self.__money = money
+
+    def get_money(self):
+        return self.__money
+
+    def add_money(self, mn):
+        self.__money += mn.__money
+
+    @classmethod
+    def __check_money(cls, money):
+        return type(money) == int and 0 <= money
+
+
+mn_1 = Money(10)
+mn_2 = Money(20)
+mn_1.set_money(100)
+mn_2.add_money(mn_1)
+m1 = mn_1.get_money()    # 100
+m2 = mn_2.get_money()    # 120
+print(m1, m2)
 
 
 # Подвиг 6. Объявите класс Book со следующим набором сеттеров и геттеров:
@@ -116,29 +116,29 @@
 # P.S. В программе требуется объявить только класс. Ничего на экран выводить не нужно.
 
 
-# class Book:
-#     def __init__(self, author, title, price):
-#         self.__author = author
-#         self.__title = title
-#         self.__price = price
-#
-#     def set_title(self, title):
-#         self.__title = title
-#
-#     def set_author(self, author):
-#         self.__author = author
-#
-#     def set_price(self, price):
-#         self.__price = price
-#
-#     def get_title(self):
-#         return self.__title
-#
-#     def get_author(self):
-#         return self.__author
-#
-#     def get_price(self):
-#         return self.__price
+class Book:
+    def __init__(self, author, title, price):
+        self.__author = author
+        self.__title = title
+        self.__price = price
+
+    def set_title(self, title):
+        self.__title = title
+
+    def set_author(self, author):
+        self.__author = author
+
+    def set_price(self, price):
+        self.__price = price
+
+    def get_title(self):
+        return self.__title
+
+    def get_author(self):
+        return self.__author
+
+    def get_price(self):
+        return self.__price
 
 
 # Подвиг 7. Объявите класс Line для описания линии на плоскости, объекты которого предполагается создавать командой:
@@ -161,25 +161,25 @@
 # P.S. В программе требуется объявить только класс. Ничего на экран выводить не нужно.
 
 
-# class Line:
-#     def __init__(self, x1, y1, x2, y2):
-#         self.set_coords(x1, y1, x2, y2)
-#
-#     def set_coords(self, x1, y1, x2, y2):
-#         self.__x1 = x1
-#         self.__x2 = x2
-#         self.__y1 = y1
-#         self.__y2 = y2
-#
-#     def get_coords(self):
-#         return self.__x1, self.__y1, self.__x2, self.__y2
-#
-#     def draw(self):
-#         print(*self.get_coords())
-#
-#
-# ex = Line(1, 2, 3, 4)
-# ex.draw()
+class Line:
+    def __init__(self, x1, y1, x2, y2):
+        self.set_coords(x1, y1, x2, y2)
+
+    def set_coords(self, x1, y1, x2, y2):
+        self.__x1 = x1
+        self.__x2 = x2
+        self.__y1 = y1
+        self.__y2 = y2
+
+    def get_coords(self):
+        return self.__x1, self.__y1, self.__x2, self.__y2
+
+    def draw(self):
+        print(*self.get_coords())
+
+
+ex = Line(1, 2, 3, 4)
+ex.draw()
 
 
 # Подвиг 8. Объявите в программе два класса Point и Rectangle. Объекты первого класса должны создаваться командой:
@@ -220,48 +220,48 @@
 # P.S. На экран ничего выводить не нужно.
 
 
-# class Point:
-#     def __init__(self, x, y):
-#         if self.check_value(x, y):
-#             self.__x = x
-#             self.__y = y
-#         else:
-#             raise ValueError('Должны быть числовые значения')
-#
-#     @staticmethod
-#     def check_value(x, y):
-#         return type(x) in (int, float) and type(y) in (int, float)
-#
-#     def get_coords(self):
-#         return self.__x, self.__y
-#
-#
-# class Rectangle:
-#     def __init__(self, *args):
-#         if len(args) == 2:
-#             self.__sp = args[0]
-#             self.__ep = args[1]
-#         elif len(args) == 4:
-#             x1, y1, x2, y2 = args
-#             self.__sp = Point(x1, y1)
-#             self.__ep = Point(x2, y2)
-#
-#     def set_coords(self, sp, ep):
-#         self.__sp = sp
-#         self.__ep = ep
-#
-#     def get_coords(self):
-#         return self.__sp, self.__ep
-#
-#     def draw(self):
-#         print(f"Прямоугольник с координатами: {self.__sp.get_coords()} {self.__ep.get_coords()}")
-#
-#
-# rect = Rectangle(0, 0, 20, 34)
-# rect.draw()
-#
-# r1 = Rectangle(Point(1, 2), Point(3, 4))
-# r1.draw()
+class Point:
+    def __init__(self, x, y):
+        if self.check_value(x, y):
+            self.__x = x
+            self.__y = y
+        else:
+            raise ValueError('Должны быть числовые значения')
+
+    @staticmethod
+    def check_value(x, y):
+        return type(x) in (int, float) and type(y) in (int, float)
+
+    def get_coords(self):
+        return self.__x, self.__y
+
+
+class Rectangle:
+    def __init__(self, *args):
+        if len(args) == 2:
+            self.__sp = args[0]
+            self.__ep = args[1]
+        elif len(args) == 4:
+            x1, y1, x2, y2 = args
+            self.__sp = Point(x1, y1)
+            self.__ep = Point(x2, y2)
+
+    def set_coords(self, sp, ep):
+        self.__sp = sp
+        self.__ep = ep
+
+    def get_coords(self):
+        return self.__sp, self.__ep
+
+    def draw(self):
+        print(f"Прямоугольник с координатами: {self.__sp.get_coords()} {self.__ep.get_coords()}")
+
+
+rect = Rectangle(0, 0, 20, 34)
+rect.draw()
+
+r1 = Rectangle(Point(1, 2), Point(3, 4))
+r1.draw()
 
 
 # Большой подвиг 9. Необходимо реализовать связный список
@@ -312,71 +312,71 @@
 # P.S. На экран ничего выводить не нужно.
 
 
-# class LinkedList:
-#     def __init__(self, head=None, tail=None):
-#         self.head = head
-#         self.tail = tail
-#
-#     def add_obj(self, obj):
-#         if self.head is None:
-#             self.head = obj
-#
-#         elif self.tail is None:
-#             self.tail = obj
-#             obj.set_prev(self.head)
-#             self.head.set_next(obj)
-#
-#         else:
-#             obj.set_prev(self.tail)
-#             self.tail.set_next(obj)
-#             self.tail = obj
-#
-#     def remove_obj(self):
-#         if self.tail is None:
-#             self.head = None
-#         else:
-#             ptr = self.tail.get_prev()
-#             ptr.set_next(None)
-#             self.tail = ptr
-#
-#     def get_data(self):
-#         res = []
-#         current = self.head
-#         while current:
-#             res.append(current.get_data())
-#             current = current.get_next()
-#         return res
-#
-# class ObjList:
-#     def __init__(self, data, next=None, prev=None):
-#         self.__next = next
-#         self.__prev = prev
-#         self.__data = data
-#
-#     def set_next(self, obj):
-#         self.__next = obj
-#
-#     def set_prev(self, obj):
-#         self.__prev = obj
-#
-#     def get_next(self):
-#         return self.__next
-#
-#     def get_prev(self):
-#         return self.__prev
-#
-#     def set_data(self, data):
-#         self.__data = data
-#
-#     def get_data(self):
-#         return self.__data
-#
-#
-# lst = LinkedList()
-# lst.add_obj(ObjList("данные 1"))
-# lst.add_obj(ObjList("данные 2"))
-# lst.add_obj(ObjList("данные 3"))
-# res = lst.get_data()  # ['данные 1', 'данные 2', 'данные 3']
+class LinkedList:
+    def __init__(self, head=None, tail=None):
+        self.head = head
+        self.tail = tail
+
+    def add_obj(self, obj):
+        if self.head is None:
+            self.head = obj
+
+        elif self.tail is None:
+            self.tail = obj
+            obj.set_prev(self.head)
+            self.head.set_next(obj)
+
+        else:
+            obj.set_prev(self.tail)
+            self.tail.set_next(obj)
+            self.tail = obj
+
+    def remove_obj(self):
+        if self.tail is None:
+            self.head = None
+        else:
+            ptr = self.tail.get_prev()
+            ptr.set_next(None)
+            self.tail = ptr
+
+    def get_data(self):
+        res = []
+        current = self.head
+        while current:
+            res.append(current.get_data())
+            current = current.get_next()
+        return res
+
+class ObjList:
+    def __init__(self, data, next=None, prev=None):
+        self.__next = next
+        self.__prev = prev
+        self.__data = data
+
+    def set_next(self, obj):
+        self.__next = obj
+
+    def set_prev(self, obj):
+        self.__prev = obj
+
+    def get_next(self):
+        return self.__next
+
+    def get_prev(self):
+        return self.__prev
+
+    def set_data(self, data):
+        self.__data = data
+
+    def get_data(self):
+        return self.__data
+
+
+lst = LinkedList()
+lst.add_obj(ObjList("данные 1"))
+lst.add_obj(ObjList("данные 2"))
+lst.add_obj(ObjList("данные 3"))
+res = lst.get_data()  # ['данные 1', 'данные 2', 'данные 3']
 
 
 # Подвиг 10 (на повторение). Объявите класс EmailValidator для проверки корректности email-адреса.
@@ -412,34 +412,34 @@
 # res = EmailValidator.check_email("sc_lib@list_ru") # False
 # P.S. В программе требуется объявить только класс. На экран ничего выводить не нужно.
 
-# import random
-# from string import digits, ascii_lowercase
-#
-#
-# class EmailValidator:
-#     st = digits + ascii_lowercase + '_.@'
-#     st_for_gen_email = digits + ascii_lowercase + '_.'
-#
-#     def __new__(cls, *args, **kwargs):
-#         return None
-#
-#     @classmethod
-#     def get_random_email(cls):
-#         res = f'{"".join([cls.st_for_gen_email[random.randint(0, len(cls.st_for_gen_email) - 1)] for x in range(10)])}@gmail.com'
-#         return res
-#
-#     @classmethod
-#     def check_email(cls, email):
-#         if cls.__is_email_str(email):
-#             if (set(email) <= set(cls.st) and len(email[0:email.index('@') - 1]) <= 100 and email.count('@') == 1 and
-#                     len(email[email.index('@'):]) <= 50 and '.' in email[email.index('@'):] and '..' not in email):
-#                 return True
-#         return False
-#
-#     @staticmethod
-#     def __is_email_str(email):
-#         return isinstance(email, str)
-#
-#
-# print(EmailValidator.check_email("sc_lib@list.ru"))
-# print(EmailValidator.check_email("sc_lib@list_ru")) # False
+import random
+from string import digits, ascii_lowercase
+
+
+class EmailValidator:
+    st = digits + ascii_lowercase + '_.@'
+    st_for_gen_email = digits + ascii_lowercase + '_.'
+
+    def __new__(cls, *args, **kwargs):
+        return None
+
+    @classmethod
+    def get_random_email(cls):
+        res = f'{"".join([cls.st_for_gen_email[random.randint(0, len(cls.st_for_gen_email) - 1)] for x in range(10)])}@gmail.com'
+        return res
+
+    @classmethod
+    def check_email(cls, email):
+        if cls.__is_email_str(email):
+            if (set(email) <= set(cls.st) and len(email[0:email.index('@') - 1]) <= 100 and email.count('@') == 1 and
+                    len(email[email.index('@'):]) <= 50 and '.' in email[email.index('@'):] and '..' not in email):
+                return True
+        return False
+
+    @staticmethod
+    def __is_email_str(email):
+        return isinstance(email, str)
+
+
+print(EmailValidator.check_email("sc_lib@list.ru"))
+print(EmailValidator.check_email("sc_lib@list_ru")) # False
